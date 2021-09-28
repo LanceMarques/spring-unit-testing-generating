2bf8f2d8c71d5ed71db1e10656426ce5892ffb68
lancemarques <luislancelote@gmail.com>
2021-09-27 23:05:20 -0300

    Adding model size validation.

--- src/main/java/com/tcc/springunittestinggenerating/entity/EntityController.java
+++ src/main/java/com/tcc/springunittestinggenerating/entity/EntityController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -27,7 +28,7 @@ public class EntityController {
     }
 
     @PostMapping
-    public ResponseEntity<EntityModel> criar (@RequestBody final EntityModel newModel){
+    public ResponseEntity<EntityModel> criar (@RequestBody @Valid final EntityModel newModel){
         final EntityModel model = this.service.criar(newModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(model);
     }
--- src/main/java/com/tcc/springunittestinggenerating/entity/EntityModel.java
+++ src/main/java/com/tcc/springunittestinggenerating/entity/EntityModel.java
@@ -1,21 +1,29 @@
 package com.tcc.springunittestinggenerating.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "entity")
 public class EntityModel {
 
     @Id
-    Integer id;
+    private Integer id;
+
+    @Size(min = 5, max = 50)
+    @Column(name = "name")
+    private String name;
 
     public EntityModel() {
     }
 
-    public EntityModel(Integer id) {
+    public EntityModel(Integer id, String name) {
         this.id = id;
+        this.name = name;
     }
 
     public Integer getId() {
@@ -24,6 +32,14 @@ public class EntityModel {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
 }
